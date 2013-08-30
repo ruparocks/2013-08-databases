8dc87832fae96c68153fdf1c0e469c180f7d242b
Hack Reactor Students <github@hackreactor.com>
2013-08-30 14:20:15 -0700

    create tables for chat server

--- SQL/schema.sql
+++ SQL/schema.sql
@@ -1,9 +1,26 @@
-CREATE DATABASE chat;
+CREATE database chat;
 
 USE chat;
 
 CREATE TABLE messages (
- /* Describe your table here.*/
+  id INT AUTO_INCREMENT,
+  text TEXT(160),
+  created_At TIMESTAMP,
+  room_id INT,
+  user_id INT,
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE rooms (
+  id INT AUTO_INCREMENT,
+  name VARCHAR(20),
+  PRIMARY KEY(id)
+);
+
+CREATE TABLE users (
+  id INT AUTO_INCREMENT,
+  username VARCHAR(20),
+  PRIMARY KEY (id)
 );
 
 /* You can also create more tables, if you need them... */
